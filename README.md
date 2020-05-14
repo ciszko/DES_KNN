d3d577f28eed0b498a0c4af72f2512d1db95d514
ciszko <242032@student.pwr.edu.pl>
2020-05-14 23:17:43 +0200

    DES_KNN Implemented

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
 # DES_KNN
+Dynamic Ensemble Selection K-nearest Neighbours Classifier project for my university.
+
+
+# Usage
+
+DES_KNN supports similiar methods to sklearn classifiers such as: `predict()` and  `fit()`. It uses sklearn built-in classifier `DecisionTreeClassifier()` to make predictions but the base classifier can be changed to everything that supports two mentioned methods.
+
+## Test
+
+In /results folder you can find statistic test between this implementation of `DES_KNN`, `deslib.DESKNN`, `deslib.KNORAU`, `deslib.KNORAE` and `sklearn.ADABoost`
+
+## Example test resuts
+
+SCORES:
+
+|  Classifier| Mean accuracy |
+|--|--|
+|  My DES_KNN| 0.848551 |
+| DES_KNN | 0.846377 |
+|  KNORA-U |  0.85 |
+|KNORA-E |0.828986|
+|ADABoost  |0.839855|
+
+---------
+
+t-statistic:
+
+|          | My DeS_KNN   | DES_KNN  |  KNORA-U   | KNORA-E  |  ADABoost|
+|---------- | ------------ | ---------|  --------- | ---------  |----------
+|My DES_KNN  |        0.00    |   0.13   |   -0.09    |   1.16     |   0.55
+|DES_KNN     |       -0.13     |  0.00   |   -0.23   |    1.07      |  0.43
+|KNORA-U     |        0.09   |    0.23   |    0.00     |  1.30      |  0.68
+|KNORA-E       |     -1.16    |  -1.07   |   -1.30     |  0.00      | -0.69
+|ADABoost     |      -0.55    |  -0.43    |  -0.68     |  0.69     |   0.00  
+
+--------
+p-value:
+
+   |  |    My DeS_KNN  |  DES_KNN   | KNORA-U  |  KNORA-E   | ADABoost|
+|---------- | ------------|  --------- | --------- | ---------  |----------|
+| My DES_KNN     |  1.00    |   0.90   |    0.93   |    0.26     |   0.59|
+|DES_KNN         |  0.90     |  1.00     |  0.82   |    0.30  |      0.67|
+|KNORA-U       |    0.93   |    0.82    |   1.00   |    0.21    |    0.50|
+|KNORA-E       |   0.26    |   0.30   |    0.21   |    1.00   |     0.50|
+|ADABoost    |     0.59    |   0.67    |   0.50     |  0.50      |  1.00|
+
+-----
+Advantage:
+| |  My DeS_KNN  |  DES_KNN   | KNORA-U  |  KNORA-E   | ADABoost|
+|---------- | ------------ | --------- | --------- | ---------|----------|
+|My DES_KNN   |      0     |     1      |    0      |    1      |     1|
+|DES_KNN   |   0     |     0     |     0      |    1      |     1|
+|KNORA-U    |    1      |    1    |      0       |   1     |      1|
+|KNORA-E      |     0      |    0    |      0     |     0    |       0|
+|ADABoost    |       0      |    0     |     0     |     1     |      0|
+
+---------------------
+
+Statistical significance (alpha = 0.05):
+
+ |       |       My DeS_KNN |   DES_KNN  |  KNORA-U |   KNORA-E    |ADABoost|
+|---------- | ------------ | ---------  |---------|  ---------  |----------
+|My DES_KNN    |         0  |        0      |    0      |    0     |      0
+|DES_KNN          |      0    |      0       |   0   |       0        |   0
+|KNORA-U         |       0     |     0      |    0    |      0       |    0
+|KNORA-E        |        0      |    0     |     0     |     0      |     0
+|ADABoost      |         0       |   0    |      0      |    0     |      0
+
+-----------------
+
+Statistically significantly better:
+|  | My DeS_KNN   | DES_KNN  |  KNORA-U |   KNORA-E   | ADABoost
+|---------- | ------------|  --------- | --------- | ---------  |----------|
+|My DES_KNN  |           0  |        0  |        0  |        0    |       0
+|DES_KNN        |        0      |    0     |     0      |    0       |    0
+|KNORA-U       |         0      |    0    |      0      |    0      |     0
+|KNORA-E       |         0       |   0    |      0       |   0      |     0
+|ADABoost     |          0        |  0   |       0        |  0     |      0
+
---
+++ des_knn.py
@@ -0,0 +1,319 @@
+import numpy as np
+import random
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.base import clone
+from math import sqrt
+
+
+class DES_KNN(object):
+    """This method selects an ensemble of classifiers. First of all it uses bagging to fit the data to base classifiers. 
+    Then K-nearest neighbors are selected for the point. All classifiers are next fed with the neighbors in order to 
+    calculate diversity and accuracy. N most accurate classifiers are selected. From them J most diverse are selected
+    to create final ensemble that can predict the sample based on majority voting
+
+    Arguments:
+        base_estimator {BaseEstimator} -- base estimator that will be later used for prediction
+        n_estimators {int} -- number of base estimators
+        k {int} -- number of neighbors to estimate competence region
+        N {int/float} -- int: number of N most accurate classifiers float: percentage of most accurate classifiers
+        J {int/float} -- int: number of J most diverse classifiers float: percentage of most accurate classifiers
+        max_samples {int/float} -- maximum samples amount to choose for bagging
+        div_method {string} -- method to calculate diversity between pair of classifiers. Supported: Q, double-fault
+        knn_metrics {string} -- metrics to calculate distance between samples. Supported: euclidean, manhattan, q(correalation coefficient), disagreement
+        random_state {int} -- random_state to pass to base_estimator
+    """
+
+    def __init__(
+        self,
+        base_estimator=DecisionTreeClassifier(),
+        n_estimators=10,
+        k=7,
+        N=0.5,
+        J=0.5,
+        max_samples=0.1,
+        div_method="double-fault",
+        knn_metrics="euclidean",
+        random_state=66,
+    ):
+
+        self.base_estimator = base_estimator
+        self.n_estimators = n_estimators
+        self.k = k  # number of neighbors
+        self.J = J  # J most diverse classifiers
+        self.N = N  # N most accurate classifiers (J < N)
+        self.max_samples = max_samples  # bagging
+        self.div_method = div_method
+        self.knn_metrics = knn_metrics
+        self.random_state = random_state
+
+    def fit(self, X, Y):
+        # copy the dataset
+        self.X = X
+        self.Y = Y
+        # check the shape
+        if len(X) != len(Y):
+            raise ValueError("Wrong shape")
+        self.estimators_ = []
+        # bagging
+        for i in range(self.n_estimators):
+            # datasets to feed estimators
+            X_ = []
+            Y_ = []
+            # if max_samples is int then choose max_samples samples
+            if type(self.max_samples) is int:
+                for i in range(self.max_samples):
+                    seed = random.randint(0, len(X) - 1)
+                    X_.append(X[seed])
+                    Y_.append(Y[seed])
+            # if max_samples is float choose percentage of samples
+            elif type(self.max_samples) is float:
+                for i in range(int(self.max_samples * len(X))):
+                    seed = random.randint(0, len(X) - 1)
+                    X_.append(X[seed])
+                    Y_.append(Y[seed])
+            # create estimator pool
+            self.base_estimator.random_state = self.random_state
+            self.estimators_.append(clone(self.base_estimator).fit(X_, Y_))
+
+    def find_k_nn(self, point, neighbors=None, metrics=None):
+        """Function finds "neighbors" nearest neighbors based on the given point and metrics
+
+        Arguments:
+            point {array} -- neighbors are found based on it 
+
+        Keyword Arguments:
+            neighbors {int} -- the amount of closest neighbors to find (default: self.k)
+            type {string} -- metrics used to determine distance (default: self.knn_metrics)
+
+        Returns:
+            list, list -- return lists of samples and coresponding classes
+        """
+        # get default args
+        if metrics is None:
+            metrics = self.knn_metrics
+        if neighbors is None:
+            neighbors = self.k
+        # all distances
+        distances = []
+        # enumerate through all samples
+        for i, row in enumerate(self.X):
+            # calculate based on choosen metrics
+            if metrics == "euclidean":
+                dist = np.linalg.norm(np.array(point) - row)
+            elif metrics == "manhattan":
+                dist = np.sum(np.abs(np.array(point) - row))
+            # append to distances
+            distances.append([i, dist])
+        # sort them based on distance
+        distances.sort(key=lambda x: x[1])
+        # choose k nearest ones
+        neighbors = distances[:neighbors]
+        # returns indices of k nearest neighbors
+        x = [self.X[item[0]] for item in neighbors]
+        y = [self.Y[item[0]] for item in neighbors]
+
+        return x, y
+
+    def calculate_accuracy(self, classifier, Xsamples, Ysamples):
+        """Calculates the accuracy of given classifier
+
+        Arguments:
+            classifier {BaseEstimator} -- classifier which accuracy will be calculated
+            Xsamples {np.array} -- list of samples
+            Ysamples {np.array} -- list of corresponding classes
+
+        Returns:
+            float -- returns accuracy of classifier 
+        """
+        TP = 0  # True Positive
+        TN = 0  # True Negative
+        FP = 0  # False Positive
+        FN = 0  # False Negative
+        # iterate over samples
+        for i, sample in enumerate(Xsamples):
+            # predict the sample
+            pred = classifier.predict(sample.reshape(1, -1))
+            # choose right case
+            if pred == Ysamples[i] == 1:
+                TP += 1
+            elif pred == 0 and Ysamples[i] == 1:
+                FN += 1
+            elif pred == 1 and Ysamples[i] == 0:
+                FP += 1
+            elif pred == Ysamples[i] == 0:
+                TN += 1
+        # calculate accuracy
+        accuracy = (TP + FN) / (TP + TN + FP + FN)
+
+        return accuracy
+
+    def calculate_pair_diversity(self, class1, class2, X, Y, method="double-fault"):
+        """Calculates diversity between two classifiers based on selected method
+
+        Arguments:
+            class1 {BaseEstimator} -- 1st estimator
+            class2 {BaseEstimator} -- 2nd estimator
+            X {np.array} -- samples based on which diversity will be calculated
+            Y {np.array} -- corresponding classes
+
+        Keyword Arguments:
+            method {str} -- method of calculating diversity (default: {"double-fault"})
+
+        Returns:
+            float -- return the diversity between two classifiers
+        """
+        N11 = 0  # both right
+        N10 = 0  # 1 right, 2 wrong
+        N01 = 0  # 1 wrong, 2 right
+        N00 = 0  # both wrong
+        for i, sample in enumerate(X):
+            # predict by both classifiers
+            pred1 = class1.predict(sample.reshape(1, -1))
+            pred2 = class2.predict(sample.reshape(1, -1))
+            # check if predictions were correct
+            # calculate Nxx
+            if pred1 == pred2 == Y[i]:
+                N11 += 1
+            elif pred1 == Y[i] and pred2 != Y[i]:
+                N10 += 1
+            elif pred1 != Y[i] and pred2 == Y[i]:
+                N01 += 1
+            elif pred1 == pred2 != Y[i]:
+                N00 += 1
+        # calculate based on given method
+        if method == "Q":
+            diversity = ((N11 * N00) - (N01 * N10)) / ((N11 * N00) + (N01 * N10))
+        if method == "double-fault":
+            diversity = N00 / (N11 + N01 + N10 + N00)
+        if method == "p":
+            diversity = (N11 * N00 - N01 * N10) / sqrt(
+                (N11 + N10) * (N01 + N00) * (N11 + N01) * (N10 + N00)
+            )
+        if method == "disagreement":
+            diversity = (N01 + N10) / (N11 + N10 + N01 + N00)
+
+        return diversity
+
+    def diversity_matrix(self, classifier_pool, X, Y, method="double-fault"):
+        """Generates diversity matrix, of size len(classifier_pool) x len(classifier_pool) with None on diagonal
+
+        Arguments:
+            classifier_pool {np.array} -- array of estimators that support predict function
+            X {np.array} -- samples on which diversity will be calculated
+            Y {np.array} -- corresponding classes
+
+        Keyword Arguments:
+            method {str} -- method passed on to calculate_pair_diversity function (default: {"double-fault"})
+
+        Returns:
+            np.array -- Return a matrix of diversities between every classifier
+        """
+        # initialize matrix of size len(classifier_pool) x len(classifier_pool) filled with None
+        div_matrix = [
+            [None] * len(classifier_pool) for i in range(len(classifier_pool))
+        ]
+        # iterate over classifiers
+        for i, cls in enumerate(classifier_pool):
+            # iterate over every other classifier
+            for j, other_cls in enumerate(classifier_pool):
+                if cls is not other_cls:
+                    # calculate diversity only above diagonal because pair diversity
+                    # between 1 and 2 is equal to diversity between 2 and 1
+                    if div_matrix[i][j] == None or div_matrix[j][i] == None:
+                        res = self.calculate_pair_diversity(
+                            cls, other_cls, X, Y, method
+                        )
+                        # fill upper and lower half of matrix
+                        div_matrix[i][j] = res
+                        div_matrix[j][i] = res
+        return div_matrix
+
+    def div_matrix_to_divs(self, div_matrix):
+        """helper function to convert diversity matrix to array of mean diversities
+
+        Arguments:
+            div_matrix {np.array} -- matrix of diversities
+
+        Returns:
+            list -- returns list of mean diversities
+        """
+        divs = []
+        for i, row in enumerate(np.array(div_matrix)):
+            # calculate mean diversity between classifier and every other classifier
+            divs.append(row[row != None].mean())
+        return divs
+
+    def create_final_ensemble(self, samples, neighbors=None):
+        """Function creates the final ensemble of classifiers. They will later be used to predict
+        the class of given samples. First calculate knn, then select N most accurate ones, from them
+        select J most diverse ones.
+
+        Arguments:
+            samples {np.array} -- array of samples based on which ensamble is created 
+
+        Keyword Arguments:
+            neighbors {int} -- amount of neighbors, passed to knn algorithm (default: {None})
+
+        Returns:
+            list(BaseEstimator) -- returns list of N most accurate and J most diverse base estimators
+        """
+        if neighbors is None:
+            neighbors = self.k
+        # knn
+        x, y = self.find_k_nn(samples, neighbors)
+        # create list of accuracies
+        accuracies = []
+        for estimator in self.estimators_:
+            # calcute accuracy of every estimator
+            accuracies.append(self.calculate_accuracy(estimator, x, y))
+        # find indices of N most accurate ones
+        if type(self.N) is int:
+            ind = np.argpartition(accuracies, int(-self.N))[int(-self.N) :]
+        elif type(self.N) is float:
+            ind = np.argpartition(accuracies, int(-self.N * self.n_estimators))[
+                int(-self.N * self.n_estimators) :
+            ]
+        # create an ensemble of N most accurate classifiers
+        ensemble_ = []
+        for index in ind:
+            ensemble_.append(self.estimators_[index])
+        # calculate diversity
+        div_matrix = self.diversity_matrix(ensemble_, x, y)
+        # convert matrix to list of diversities
+        diversities = self.div_matrix_to_divs(div_matrix)
+        # find indices of J most diverse ones
+        if type(self.J) is int:
+            ind = np.argpartition(diversities, int(-self.J))[int(-self.J) :]
+        elif type(self.J) is float:
+            ind = np.argpartition(diversities, int(-self.J * self.n_estimators))[
+                int(-self.J * self.n_estimators) :
+            ]
+        # create final ensemble
+        final_ensemble = []
+        for index in ind:
+            final_ensemble.append(ensemble_[index])
+        return final_ensemble
+
+    def predict(self, samples):
+        """Predicts classes of given samples
+
+        Arguments:
+            samples {np.array} -- array of samples to predict
+
+        Returns:
+            list -- returns list of predicted classes
+        """
+        # create classes list to return
+        classes = []
+        # iterate over samples
+        for sample in samples:
+            # for every one create an ensemble of classifiers
+            self.ensemble_ = self.create_final_ensemble(sample)
+            # predict with every classifier from ensemble
+            predicts = []
+            for clas in self.ensemble_:
+                predicts.append(clas.predict(sample.reshape(1, -1)).tolist())
+            # find most common occuring class from predictions
+            counter = sum(predicts, [])
+            classes.append(max(set(list(counter)), key=counter.count))
+        return np.array(classes)
